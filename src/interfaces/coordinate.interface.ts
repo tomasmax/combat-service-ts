b0defa5ec6cfa8166187495fa96fe3e0da1c9eff
Tomas Madariaga <critimass@gmail.com>
2023-02-03 08:35:59 +0100

    remove no needed code after refactors

--- src/interfaces/coordinate.interface.ts
+++ src/interfaces/coordinate.interface.ts
@@ -1,5 +1,4 @@
 export interface Coordinate {
   x: number;
   y: number;
-  calculateDistance?(coordinate?: Coordinate): number;
 }
--- src/models/coordinate.model.ts
+++ src/models/coordinate.model.ts
@@ -8,29 +8,6 @@ class CoordinateModel implements Coordinate {
     this.x = x;
     this.y = y;
   }
-
-  calculateDistance(coordinate: Coordinate = { x: 0, y: 0 }): number {
-    return Math.sqrt(Math.pow(Math.abs(coordinate.x - this.x), 2) + Math.pow(Math.abs(coordinate.y - this.y), 2));
-  }
-
-  static MAX_DISTANCE = 100;
-
-  static isAPossibleTargetCoordinate({
-    isClosest,
-    currentDistance,
-    lastMaxOrMinDistance,
-  }: {
-    isClosest: boolean;
-    currentDistance: number;
-    lastMaxOrMinDistance: number;
-  }): boolean {
-    const isCloser = (currentDistance: number, minDistance: number): boolean => currentDistance < minDistance;
-
-    const isFurther = (currentDistance: number, maxDistance: number): boolean =>
-      currentDistance > maxDistance && currentDistance <= CoordinateModel.MAX_DISTANCE;
-
-    return isClosest ? isCloser(currentDistance, lastMaxOrMinDistance) : isFurther(currentDistance, lastMaxOrMinDistance);
-  }
 }
 
 export default CoordinateModel;
